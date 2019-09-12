37e167b6cee71e4a7b8ca2eae2eee9f978dae165
gordonbannier <gordonbannier>
2019-09-12 15:59:19 +0200

    writing tests (not finished)

--- src/app/components/timetracking/contracts/contract.component.spec.ts
+++ src/app/components/timetracking/contracts/contract.component.spec.ts
@@ -1,6 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContractComponent } from './contract.component';
+import {AppRoutingModule} from "../../../app.routing.module";
+import {PageNotFoundComponent} from "../../page-not-found/page-not-found.component";
+import {ProjectsComponent} from "../projects/projects.component";
+import {EntriesComponent} from "../projects/entries/entries.component";
+import {ModalComponent} from "../projects/entries/modal/modal.component";
+import {DynamicFormComponent} from "../../../common/reactive-form/dynamic-form/dynamic-form.component";
+import {DynamicFormQuestionComponent} from "../../../common/reactive-form/dynamic-form-question/dynamic-form-question.component";
+import {CommonModule} from "@angular/common";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {TimetrackingRoutingModule} from "../timetracking.routing.module";
+import {DataEntryService} from "../../../common/services/data-entry.service";
 
 describe('ContractComponent', () => {
   let component: ContractComponent;
@@ -8,7 +20,18 @@ describe('ContractComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContractComponent ]
+      declarations: [ContractComponent,
+        ProjectsComponent,
+        EntriesComponent,
+        ModalComponent,
+        DynamicFormComponent,
+        DynamicFormQuestionComponent],
+      imports: [CommonModule,
+        FormsModule,
+        NgbModule,
+        TimetrackingRoutingModule,
+        ReactiveFormsModule],
+      providers: [DataEntryService]
     })
     .compileComponents();
   }));
